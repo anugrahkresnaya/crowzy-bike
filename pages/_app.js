2a40d3262006fca73a50047849efab6f02d4e99f
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2023-01-09 21:16:33 +0700

    refactor: delete unused codes and add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const colors = {
+    brand: {
+      900: '#051014',
+      800: '#2e2f2f',
+      700: '#a599b5',
+      600: '#cddddd',
+      500: '#acbdba',
+    },
+  }
+
+  const theme = extendTheme({ colors })
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
